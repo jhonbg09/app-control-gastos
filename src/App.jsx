d4c7a8e113913286ddc1b3d9ce0e125e745096de
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-27 14:15:33 -0500

    creando funcion handle para el boton modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import IconoNuevoGasto from "./img/nuevo-gasto.svg"
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+  const [modal, setModal] = useState(false)
+  const handleNuevoGasto =() => {
+    setModal(true);
+  }
   return (
     <div>
       <Header
@@ -12,15 +16,18 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
-      
+
       {isValidPresupuesto && (
       <div className="nuevo-gasto">
         <img 
           src={IconoNuevoGasto}
           alt="icono nuevo gasto"
+          onClick={handleNuevoGasto}
         />
       </div>
       )}
+
+      {modal && <p>Desde Modal</p>}
     </div>
   );
 }
