d9cca57fbc94bc02802b9631babbf2d309b8655f
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-28 11:56:16 -0500

    creando los state para los gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,52 +1,70 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import CerrarBtn from '../img/cerrar.svg'
-const Modal = ({setModal, animarModal, setAnimarModal}) => {
-    const ocultarModal =() => {
-        
-        setAnimarModal(false)
+import CerrarBtn from "../img/cerrar.svg";
+import { useState } from "react";
 
-        setTimeout(() => {
-            setModal(false)
-        }, 500);
-    }
+const Modal = ({ setModal, animarModal, setAnimarModal }) => {
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoia] = useState('')
+  const ocultarModal = () => {
+    setAnimarModal(false);
+
+    setTimeout(() => {
+      setModal(false);
+    }, 500);
+  };
 
   return (
     <div className="modal">
       <p>Desde Modal</p>
       <div className="cerrar-modal">
-        <img
-            src={CerrarBtn}
-            alt='boto cerrar'
-            onClick={ocultarModal}
-        />
+        <img src={CerrarBtn} alt="boto cerrar" onClick={ocultarModal} />
       </div>
-      <form className={`formulario ${animarModal ? 'animar': 'cerrar'}`}>
+      <form className={`formulario ${animarModal ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
-        <div className='campo'>
-            <label htmlFor='nombre'>Nombre Gasto</label>
-            <input type='text' id='nombre' name='nombre' placeholder='Añade el Nombre del Gasto'/>
+
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+          <input
+            type="text"
+            id="nombre"
+            name="nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            placeholder="Añade el Nombre del Gasto"
+          />
         </div>
-        <div className='campo'>
-            <label htmlFor='cantidad'>Cantidad</label>
-            <input type='number' id='cantidad' name='cantidad' placeholder='Añade la Cantidad del Gasto ej: 100'/>
+
+        <div className="campo">
+          <label htmlFor="cantidad">Cantidad</label>
+          <input
+            type="number"
+            id="cantidad"
+            name="cantidad"
+            placeholder="Añade la Cantidad del Gasto ej: 100"
+            value={cantidad}
+            onChange={e => setCantidad(Number(e.target.value))}
+          />
         </div>
-        <div className='campo'>
-            <label htmlFor='categoria'>Cantegoria</label>
-            <select id='categoria'>
-                <option value=''>-- Seleccione --</option>
-                <option value='ahorro'>Ahorro</option>
-                <option value='comida'>Comida</option>
-                <option value='casa'>Casa</option>
-                <option value='gastos'>Gastos Varios</option>
-                <option value='ocio'>Ocio</option>
-                <option value='salud'>Salud</option>
-                <option value='suscripciones'>Suscripciones</option>
-            </select>
+        <div className="campo">
+          <label htmlFor="categoria">Cantegoria</label>
+          <select id="categoria" value={categoria} onChange={e => setCategoia(e.target.value)}>
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
         </div>
-        <input type='submit' value='Añadir Gasto'/>
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
