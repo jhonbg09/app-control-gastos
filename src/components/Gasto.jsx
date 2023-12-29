3bbefeb6852554a87ddf5af246bc6627ffa25665
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-28 22:37:18 -0500

    Mostrando iconos p1

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,9 +1,17 @@
 /* eslint-disable react/prop-types */
 import { FormatearFecha } from '../helpers/index'
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
 const Gasto = ({gasto, key}) => {
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+      {}
         <div className="descripcion-gasto">
           <p className="categoria">{gasto.categoria}</p>
           <p className="nombre-gasto">{gasto.nombre}</p>
