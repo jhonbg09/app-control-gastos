4f558a28b01323e7d4a98becdc800f6cb56cebe2
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-28 22:43:24 -0500

    Iconos p2

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,26 +1,42 @@
 /* eslint-disable react/prop-types */
-import { FormatearFecha } from '../helpers/index'
-import IconoAhorro from '../img/icono_ahorro.svg'
-import IconoCasa from '../img/icono_casa.svg'
-import IconoComida from '../img/icono_comida.svg'
-import IconoGastos from '../img/icono_gastos.svg'
-import IconoOcio from '../img/icono_ocio.svg'
-import IconoSalud from '../img/icono_salud.svg'
-import IconoSuscripciones from '../img/icono_suscripciones.svg'
-const Gasto = ({gasto, key}) => {
+import { FormatearFecha } from "../helpers/index";
+import IconoAhorro from "../img/icono_ahorro.svg";
+import IconoCasa from "../img/icono_casa.svg";
+import IconoComida from "../img/icono_comida.svg";
+import IconoGastos from "../img/icono_gastos.svg";
+import IconoOcio from "../img/icono_ocio.svg";
+import IconoSalud from "../img/icono_salud.svg";
+import IconoSuscripciones from "../img/icono_suscripciones.svg";
+
+const diccionarioIconos = {
+  ahorro: IconoAhorro,
+  comida: IconoComida,
+  casa: IconoCasa,
+  gastos: IconoGastos,
+  ocio: IconoOcio,
+  salud: IconoSalud,
+  suscripciones: IconoSuscripciones,
+};
+
+const Gasto = ({ gasto, key }) => {
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-      {}
+        <img
+          src={diccionarioIconos[gasto.categoria]}
+          alt={gasto.categoria}
+        />
         <div className="descripcion-gasto">
           <p className="categoria">{gasto.categoria}</p>
           <p className="nombre-gasto">{gasto.nombre}</p>
-          <p className="fecha-gasto">Agregado el: {' '} <span>{FormatearFecha(gasto.fecha) }</span></p>
+          <p className="fecha-gasto">
+            Agregado el: <span>{FormatearFecha(gasto.fecha)}</span>
+          </p>
         </div>
       </div>
-          <p className="cantidad-gasto">${gasto.cantidad}</p>
+      <p className="cantidad-gasto">${gasto.cantidad}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Gasto
+export default Gasto;
