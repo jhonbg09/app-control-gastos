c3d4da2a5d7569b6f04035b113236d92cf42bb64
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-29 12:07:40 -0500

    Calculando disonible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -7,6 +7,8 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
 
   useEffect(() => {
     const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
+    const totalDisponible = presupuesto - totalGastado
+    setDisponible(totalDisponible)
     setGastado(totalGastado) 
   },[gastos])
 
