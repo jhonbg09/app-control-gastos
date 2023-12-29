a0f2ad7d3eac7b87691d25951e3cec834af10345
jhonbg09 <jhon.betancourthg09@gmail.com>
2023-12-28 21:38:29 -0500

    Mostrando los gastos

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,7 +1,13 @@
+/* eslint-disable react/prop-types */
 const Gasto = ({gasto, key}) => {
   return (
-    <div>
-      <p>Desde Gasto</p>
+    <div className="gasto sombra">
+      <div className="contenido-gasto">
+        <div className="descripcion-gasto">
+          <p className="categoria">{gasto.categoria}</p>
+          <p className="nombre-gasto">{gasto.nombre}</p>
+        </div>
+      </div>
     </div>
   )
 }
