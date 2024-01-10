383b40be5a058123d4ef9bb3c331f632d7bcd112
jhonbg09 <jhon.betancourthg09@gmail.com>
2024-01-10 14:53:26 -0500

    Estilizando grafica

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,47 +1,67 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import { CircularProgressbar } from "react-circular-progressbar"
-import "react-circular-progressbar/dist/styles.css"
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-const ControlPresupuesto = ({presupuesto, gastos}) => {
-  const [disponible, setDisponible] = useState(0)
-  const [gastado, setGastado] = useState(0)
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
+  const [porcentaje, setPorcentaje] = useState(10);
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
 
   useEffect(() => {
-    const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0)
-    const totalDisponible = presupuesto - totalGastado
-    setDisponible(totalDisponible)
-    setGastado(totalGastado) 
-  },[gastos])
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+    const totalDisponible = presupuesto - totalGastado;
+    //Calculo de porcentaje
 
-    // Asi formateo el valor de que se ingresa
-    const formatearCantidad = (cantidad) => {
-        return cantidad.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'USD'
-        });
-    }
+    const nuevoPorcentaje = (
+      ((presupuesto - totalDisponible) / presupuesto) *
+      100
+    ).toFixed(2);
+
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
+
+    setTimeout(() => {
+      setPorcentaje(nuevoPorcentaje);
+    }, 1500);
+  }, [gastos]);
+
+  // Asi formateo el valor que se ingresa
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <CircularProgressbar
-          value={0}
+        <CircularProgressbar styles={buildStyles({
+          pathColor: '#3b82f6',
+          trailColor: '#f5f5f5',
+          textColor: '#3b82f6'
+        })} 
+        value={porcentaje} 
+        text={`${porcentaje}% Gastado `}
         />
       </div>
       <div className="contenido-presupuesto">
         <p>
-            <span>Presupuesto: </span>{' '}{formatearCantidad(presupuesto)}
+          <span>Presupuesto: </span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-            <span>Disponible: </span>{' '}{formatearCantidad(disponible)}
+          <span>Disponible: </span> {formatearCantidad(disponible)}
         </p>
         <p>
-            <span>Gastado: </span>{' '}{formatearCantidad(gastado)}
+          <span>Gastado: </span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ControlPresupuesto
+export default ControlPresupuesto;
